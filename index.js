3bac837847ada63b3fdb8fa4778e30ad13916190
CSmith <layaway-0idol@icloud.com>
2023-02-03 12:58:23 -0500

    commit tree 98571cfb3915c38056a088b343461e21f48771c6

---
+++ index.js
@@ -0,0 +1,8 @@
+function add(x, y) {return x+y;}
+function multiply(a,b) {return a*b;}
+function divide(a, b) {return a/b;}
+function subtract(a, b) {return a-b;}
+function increment(n) { return ( n + 1 ); }
+function decrement(n) { return ( n - 1); }
+function makeInt(string) {return parseInt(string, 10);}
+function preserveDecimal(string) {return (parseFloat(string));}
